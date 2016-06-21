4880722939398a5fe9bdba2bd5bc0acf4e4e9c50
groschatchauve <valentin.lesieur@gmail.com>
2016-06-21 11:33:53 +0200

    Installation Angular 2
    Mise en place d'Angular 2 pour TypeScript

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test',
+  template: ` Contenu angular chargé ` // mettre le contenu généré dynamiquement ici
+})
+export class AppComponent { }
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/

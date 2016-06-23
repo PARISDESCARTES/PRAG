3ba9793975fe8073312dba5e4945e70b06e70551
RAMONI Ted <eagleframe@gmail.com>
2016-06-23 11:27:01 +0200

    ajout menu

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 import { HTTP_PROVIDERS } from '@angular/http';
 
-import { NameListService, NavbarComponent, ToolbarComponent } from './shared/index';
+import { NameListService, TabMenuComponent, ToolbarComponent } from './shared/index';
 
 /**
  * This class represents the main application component. Within the @Routes annotation is the configuration of the
@@ -13,6 +13,6 @@ import { NameListService, NavbarComponent, ToolbarComponent } from './shared/ind
   selector: 'sd-app',
   viewProviders: [NameListService, HTTP_PROVIDERS],
   templateUrl: 'app.component.html',
-  directives: [ROUTER_DIRECTIVES, NavbarComponent, ToolbarComponent]
+  directives: [ROUTER_DIRECTIVES, TabMenuComponent, ToolbarComponent]
 })
 export class AppComponent {}
--- src/client/app/shared/navbar/navbar.component.ts
+++ src/client/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
+import {TabMenu,MenuItem} from 'primeng/primeng';
+
 
 /**
  * This class represents the navigation bar component.
@@ -9,6 +11,26 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
   selector: 'sd-navbar',
   templateUrl: 'navbar.component.html',
   styleUrls: ['navbar.component.css'],
-  directives: [ROUTER_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES, TabMenu]
 })
-export class NavbarComponent {}
+//export class NavbarComponent {}
+
+export class TabMenuComponent {
+
+  private items: MenuItem[];
+
+  private activeItem: MenuItem;
+
+  ngOnInit() {
+    this.items = [
+      {label: 'Enfant', icon: 'fa-child',  routerLink: ['/enfant']},
+      {label: 'Fil d\'actualité', icon: 'fa-user-md',  routerLink: ['/filactu']},
+      {label: 'Programme', icon: 'fa-calendar',  routerLink: ['/programme']},
+      {label: 'Compte Rendu', icon: 'fa-support',  routerLink: ['/compterendu']},
+      {label: 'Dashboard', icon: 'fa-dashboard',  routerLink: ['/dashboard']},
+      {label: 'Bilans', icon: 'fa-text-o',  routerLink: ['/bilan']}
+    ];
+
+    this.activeItem = this.items[0];
+  }
+}

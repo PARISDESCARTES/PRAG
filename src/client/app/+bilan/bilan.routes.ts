614a209b51cd855ebb8147946c9c3e3346cfd64b
hood hood <hood hood>
2016-06-23 16:02:33 +0200

    Add Bilan page and Correct html enfant page

---
+++ src/client/app/+bilan/bilan.routes.ts
@@ -0,0 +1,8 @@
+import { bilanComponent } from './index';
+
+export const bilanRoutes = [
+  {
+    path: 'bilan',
+    component: bilanComponent
+  }
+];
--- src/client/app/app.routes.ts
+++ src/client/app/app.routes.ts
@@ -6,7 +6,7 @@ import { FilActuRoutes } from './+filActu/index';
 import { ProgrammeRoutes } from './+programme/index';
 import { CompteRenduRoutes } from './+compteRendu/index';
 import { DashboardRoutes } from './+dashboard/index';
-/*import { BilansRoutes } from './+bilans/index';*/
+import { bilanRoutes } from './+bilan/index';
 
 
 
@@ -16,8 +16,8 @@ const routes: RouterConfig = [
   ...FilActuRoutes,
   ...ProgrammeRoutes,
   ...CompteRenduRoutes,
-  ...DashboardRoutes/*
-  ...BilansRoutes,*/
+  ...DashboardRoutes
+  ...bilanRoutes,
 ];
 
 export const APP_ROUTER_PROVIDERS = [

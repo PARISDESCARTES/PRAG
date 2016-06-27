db8fcc91adcb54c57be5d2cd0ebdf3154c4ab415
bylalhood <farid.ahamada25@gmail.com>
2016-06-27 16:35:15 +0200

    add title box and date in the AjouterElement Page ; (Calendar in toggle)

--- src/client/app/+ajouterElement/ajouterElement.component.ts
+++ src/client/app/+ajouterElement/ajouterElement.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { REACTIVE_FORM_DIRECTIVES } from '@angular/forms/index';
-import {Dropdown, SelectItem} from 'primeng/primeng';
+import {Dropdown, SelectItem, InputText, Calendar} from 'primeng/primeng';
 
 /**
  * This class represents the lazy loaded EnfantComponent.
@@ -10,22 +10,21 @@ import {Dropdown, SelectItem} from 'primeng/primeng';
   selector: 'sd-home',
   templateUrl: 'ajouterElement.component.html',
   styleUrls: ['ajouterElement.component.css'],
-  directives: [REACTIVE_FORM_DIRECTIVES, Dropdown]
+  directives: [REACTIVE_FORM_DIRECTIVES, Dropdown, InputText, Calendar]
 
 })
 
 export class AjouterElementComponent {
 categorie: SelectItem[];
-
-    selectedCategorie: string;
-
+  selectedCategorie: string;
+  dateValue:string;
     constructor() {
         this.categorie = [];
         this.categorie.push({label:'Bilan', value:'Bilan'});
         this.categorie.push({label:'A voir', value:'A voir'});
         this.categorie.push({label:'A voir', value:'A voir'});
         this.categorie.push({label:'A voir', value:'A voir'});
-        
+
     }
 }
 

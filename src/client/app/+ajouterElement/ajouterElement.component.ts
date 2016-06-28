290f41d8d0c8448b3da5071f6377e68a243c8985
bylalhood <farid.ahamada25@gmail.com>
2016-06-28 19:09:41 +0200

    add element on ajouter element page

--- src/client/app/+ajouterElement/ajouterElement.component.ts
+++ src/client/app/+ajouterElement/ajouterElement.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { REACTIVE_FORM_DIRECTIVES } from '@angular/forms/index';
-import {Dropdown, SelectItem, InputText, Calendar} from 'primeng/primeng';
+import {Dropdown, SelectItem, InputText, InputTextarea,Button, Calendar} from 'primeng/primeng';
 
 /**
  * This class represents the lazy loaded EnfantComponent.
@@ -10,20 +10,31 @@ import {Dropdown, SelectItem, InputText, Calendar} from 'primeng/primeng';
   selector: 'sd-home',
   templateUrl: 'ajouterElement.component.html',
   styleUrls: ['ajouterElement.component.css'],
-  directives: [REACTIVE_FORM_DIRECTIVES, Dropdown, InputText, Calendar]
+  directives: [REACTIVE_FORM_DIRECTIVES, Dropdown, InputText, InputTextarea, Button, Calendar]
 
 })
 
 export class AjouterElementComponent {
+  onclick() {
+
+  }
 categorie: SelectItem[];
-  selectedCategorie: string;
-  dateValue:string;
+cat_therapie: SelectItem[];
+
+
     constructor() {
         this.categorie = [];
+        this.categorie.push({label:'Selectionnez votre type', value:''});
         this.categorie.push({label:'Bilan', value:'Bilan'});
         this.categorie.push({label:'A voir', value:'A voir'});
         this.categorie.push({label:'A voir', value:'A voir'});
-        this.categorie.push({label:'A voir', value:'A voir'});
+
+
+        this.cat_therapie = [];
+        this.cat_therapie.push({label:'Selectionnez une thérapie', value:''});
+        this.cat_therapie.push({label:'A voir', value:'A voir'});
+        this.cat_therapie.push({label:'A voir', value:'A voir'});
+        this.cat_therapie.push({label:'A voir', value:'A voir'});
 
     }
 }

e078ca379af54f3b4c8dc2a64addd21c11de6a05
bylalhood <farid.ahamada25@gmail.com>
2016-06-29 16:59:49 +0200

    Change the html/css of ajouter element page

--- src/client/app/+ajouterElement/ajouterElement.component.ts
+++ src/client/app/+ajouterElement/ajouterElement.component.ts
@@ -1,6 +1,11 @@
-import {Component} from '@angular/core';
-import {REACTIVE_FORM_DIRECTIVES} from '@angular/forms/index';
-import {Dropdown, SelectItem, InputText, InputTextarea, Button, Calendar} from 'primeng/primeng';
+import { Component } from '@angular/core';
+import { REACTIVE_FORM_DIRECTIVES } from '@angular/forms/index';
+import {Panel} from 'primeng/primeng';
+import {SplitButton} from 'primeng/primeng';
+import {SplitButtonItem} from 'primeng/primeng';
+import {TabView} from 'primeng/primeng';
+import {TabPanel} from 'primeng/primeng';
+import {Growl} from 'primeng/primeng';
 
 /**
  * This class represents the lazy loaded EnfantComponent.
@@ -10,35 +15,9 @@ import {Dropdown, SelectItem, InputText, InputTextarea, Button, Calendar} from '
   selector: 'sd-home',
   templateUrl: 'ajouterElement.component.html',
   styleUrls: ['ajouterElement.component.css'],
-  directives: [REACTIVE_FORM_DIRECTIVES, Dropdown, InputText, InputTextarea, Button, Calendar]
+  directives: [REACTIVE_FORM_DIRECTIVES, Panel, TabView, TabPanel, Growl, SplitButton, SplitButtonItem]
 
 })
 
 export class AjouterElementComponent {
-  onclick() {
-
-  }
-
-  categorie:SelectItem[];
-  cat_therapie:SelectItem[];
-
-
-  constructor() {
-    this.categorie = [];
-    this.cat_therapie = [];
-  }
-
-  ngOnInit() {
-    this.categorie.push({label: 'Selectionnez votre type', value: ''});
-    this.categorie.push({label: 'Bilan', value: 'Bilan'});
-    this.categorie.push({label: 'A voir', value: 'A voir'});
-    this.categorie.push({label: 'A voir', value: 'A voir'});
-
-    this.cat_therapie.push({label: 'Selectionnez une thérapie', value: ''});
-    this.cat_therapie.push({label: 'A voir', value: 'A voir'});
-    this.cat_therapie.push({label: 'A voir', value: 'A voir'});
-    this.cat_therapie.push({label: 'A voir', value: 'A voir'});
-  }
-
 }
-

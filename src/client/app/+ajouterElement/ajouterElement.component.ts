110ff4f76f20cca6339a341c52f31620d81d0501
RAMONI Ted <eagleframe@gmail.com>
2016-06-29 16:08:01 +0200

    responsive grid enfant

--- src/client/app/+ajouterElement/ajouterElement.component.ts
+++ src/client/app/+ajouterElement/ajouterElement.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { REACTIVE_FORM_DIRECTIVES } from '@angular/forms/index';
-import {Dropdown, SelectItem, InputText, InputTextarea,Button, Calendar} from 'primeng/primeng';
+import {Component} from '@angular/core';
+import {REACTIVE_FORM_DIRECTIVES} from '@angular/forms/index';
+import {Dropdown, SelectItem, InputText, InputTextarea, Button, Calendar} from 'primeng/primeng';
 
 /**
  * This class represents the lazy loaded EnfantComponent.
@@ -18,24 +18,27 @@ export class AjouterElementComponent {
   onclick() {
 
   }
-categorie: SelectItem[];
-cat_therapie: SelectItem[];
+
+  categorie:SelectItem[];
+  cat_therapie:SelectItem[];
 
 
-    constructor() {
-        this.categorie = [];
-        this.categorie.push({label:'Selectionnez votre type', value:''});
-        this.categorie.push({label:'Bilan', value:'Bilan'});
-        this.categorie.push({label:'A voir', value:'A voir'});
-        this.categorie.push({label:'A voir', value:'A voir'});
+  constructor() {
+    this.categorie = [];
+    this.cat_therapie = [];
+  }
 
+  ngOnInit() {
+    this.categorie.push({label: 'Selectionnez votre type', value: ''});
+    this.categorie.push({label: 'Bilan', value: 'Bilan'});
+    this.categorie.push({label: 'A voir', value: 'A voir'});
+    this.categorie.push({label: 'A voir', value: 'A voir'});
 
-        this.cat_therapie = [];
-        this.cat_therapie.push({label:'Selectionnez une thérapie', value:''});
-        this.cat_therapie.push({label:'A voir', value:'A voir'});
-        this.cat_therapie.push({label:'A voir', value:'A voir'});
-        this.cat_therapie.push({label:'A voir', value:'A voir'});
+    this.cat_therapie.push({label: 'Selectionnez une thérapie', value: ''});
+    this.cat_therapie.push({label: 'A voir', value: 'A voir'});
+    this.cat_therapie.push({label: 'A voir', value: 'A voir'});
+    this.cat_therapie.push({label: 'A voir', value: 'A voir'});
+  }
 
-    }
 }
 
